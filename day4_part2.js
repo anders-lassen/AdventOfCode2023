9672c3d8d4b1f0049047f5fc82a892b2440b903f
ARL <arl@tracelink.dk>
2023-12-05 15:46:37 +0100

    day4

--- day4_part2.js
+++ day4_part2.js
@@ -12,36 +12,41 @@ process.stdin.on('data', function (data) {
     ready(input)
 })
 
-async function ready(input) {
-    console.log(input)
+async function ready(cards) {
+//     console.log(input)
 
-    var extracted_number = 0
+    var copies = []
 
-    for (let i = 0; i < 6/* input.length */; i++) {
-        const card = input[i];
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
         
-        console.log("i", i + 1/* , card */)
+        // console.log("i", i/* , card */)
+        
+        if (!copies[i]) {
+            copies[i] = 1
+        }
+
         var j = 0
         for (const number of card.numbers) {
             for (const winning of card.winning) {
                 if (number == winning) {
-                    j++
+                        j++
+                        
+                    copies[i+j] = copies[i] + (copies[i+j] || 1)
                 }
             }
         }
-
-        console.log("j", j)
-        for (let k = 1; k <= j; k++) {
-
-            console.log("k", k, j, i + k + 1)
-            input.splice(i + k + 1, 0, input[i + k + 1])
-        }
-
-        // extracted_number += j
     }
 
-    console.log(input.length)
+//     console.log(cards.length)
+//     console.log(copies)
+    console.log(copies.filter(Boolean).reduce((a, b) => a + b, 0))
     console.timeEnd('day4');
 }
 
-// correct answer: 
+
+// tries: 17185992 (too low)
+// tries: 19499881
+// tries: 14910101902 (nope)
+// tries: 44751276498 (too high)
+// correct answer: 19499881
